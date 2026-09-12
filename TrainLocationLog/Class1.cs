ed912e34962603968a6b72950a4c58eff4854a33
Ichihai1415 <projects3141.gif@gmail.com>
2026-09-12 23:27:50 +0900

    HFある程度

---
+++ TrainLocationLog/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrainLocationLog
+{
+
+    public class HF_car_type
+    {
+        public string dt { get; set; }
+        public Type[] type { get; set; }
+        public class Type
+        {
+            public string style_line { get; set; }
+            public string code { get; set; }
+            public string style_hrm { get; set; }
+            public string flg { get; set; }
+            public string name { get; set; }
+        }
+    }
+
+
+
+    public class HF_station
+    {
+        public Ikisaki[] ikisaki { get; set; }
+        public string dt { get; set; }
+
+        public class Ikisaki
+        {
+            public string name { get; set; }
+            public string code { get; set; }
+            public string yomi { get; set; }
+            public string eiji { get; set; }
+        }
+    }
+
+
+}
--- TrainLocationLog/Program.cs
+++ TrainLocationLog/Program.cs
@@ -1,13 +1,21 @@
-﻿using System.Text;
+﻿using System.Text.Json;
 using System.Text.Json.Nodes;
+using System.Text.RegularExpressions;
+using TrainLocationLog;
 
 var client = new HttpClient();
 
+var hf_jsonSt_carType = client.GetAsync("https://trainlocation.hapi-line.co.jp/config/car_type.json").Result;
+var hf_carType = JsonSerializer.Deserialize<HF_car_type>(hf_jsonSt_carType.Content.ReadAsStringAsync().Result);
+var hf_carType_dict = hf_carType.type.ToDictionary(x => x.code, x => x.name);
+
+var hf_jsonSt_station = client.GetAsync("https://trainlocation.hapi-line.co.jp/config/station.json").Result;
+var hf_station = JsonSerializer.Deserialize<HF_station>(hf_jsonSt_station.Content.ReadAsStringAsync().Result);
+var hf_station_dict = hf_station.ikisaki.ToDictionary(x => x.code, x => x.name);
 
 System.Timers.Timer timer;
 ScheduleNext();
 Thread.Sleep(Timeout.Infinite);
-
 
 void ScheduleNext()
 {
@@ -16,12 +24,13 @@ void ScheduleNext()
         now.Year, now.Month, now.Day,
         now.Hour, now.Minute, 15);
 
-    if (now.Second >= 15)
+    if (now.Second >= 14)//14.999とか対策で14
     {
         next = next.AddMinutes(1);
     }
 
     double wait = (next - now).TotalMilliseconds;
+    if (wait < 0) wait = 0;
 
     timer = new System.Timers.Timer(wait)
     {
@@ -29,67 +38,91 @@ void ScheduleNext()
     };
     timer.Elapsed += (_, __) =>
     {
-        Run();
-        ScheduleNext();
+        try
+        {
+            Run();
+        }
+        catch (Exception ex)
+        {
+            Console.Error.WriteLine($"Error in Run(): {ex}");
+        }
+        finally
+        {
+            ScheduleNext();
+        }
     };
     timer.Start();
 }
 
 void Run()
 {
+    HF();
+}
 
+void HF()
+{
+    /*
+     id :列番%2
+    E102D:1
+    E114D:1
+    E114U:0
+    D106:1
+    U103:0
+    E=駅停車　D:下り？U:上り
+     */
     var res = client.GetAsync("https://trainlocation.hapi-line.co.jp/data/traffic_info.json").Result;
-
-    var json = JsonObject.Parse(res.Content.ReadAsStringAsync().Result);
+    var json = JsonNode.Parse(res.Content.ReadAsStringAsync().Result);
 
     var dt = json["UP"][0]["dt"].ToString();
+    Console.Write(DateTime.Now);
+    Console.Write(" - ");
     Console.WriteLine(dt);
-    foreach (var ts in json["TS"].AsArray())
-    {
-        var id = ts["id"].ToString();
-        foreach (var tr in ts["tr"].AsArray())
-        {
-            var no = tr["no"].ToString();
-            var bs = tr["bs"].ToString();
-            var sy = tr["sy"].ToString();
-            var ik = tr["ik"].ToString();
-            var dl = tr["dl"].ToString();
-            var hk = tr["hk"].ToString();
-            var line = $"{dt},TS,{bs},{sy},{ik},{dl},{hk}\n";
-            AddCsv(no, line);
-        }
 
-    }
-    foreach (var ek in json["EK"].AsArray())
-    {
-        var id = ek["id"].ToString();
-        foreach (var tr in ek["tr"].AsArray())
+    foreach (var key in new string[] { "TS", "EK" })
+        foreach (var el in json[key]?.AsArray() ?? [])
         {
-            var no = tr["no"].ToString();
-            var bs = tr["bs"].ToString();
-            var sy = tr["sy"].ToString();
-            var ik = tr["ik"].ToString();
-            var dl = tr["dl"].ToString();
-            var hk = tr["hk"].ToString();
-            var line = $"{dt},EK,{bs},{sy},{ik},{dl},{hk}\n";
-            AddCsv(no, line);
+            var id = el["id"].ToString();
+            var isE = id.Contains('E');
+            var isU = id.Contains('U');
+            var isD = id.Contains('D');
+            var id_num = Regex.Replace(id, @"\D", "");
+            foreach (var tr in el["tr"].AsArray())
+            {
+                var no = tr["no"].ToString();
+                var bs = tr["bs"].ToString();
+                var sy = tr["sy"].ToString();
+                var ik = tr["ik"].ToString();
+                var dl = tr["dl"].ToString();
+                var hk = tr["hk"].ToString();
+                hf_carType_dict.TryGetValue(sy, out var syName);
+                hf_station_dict.TryGetValue(ik, out var ikName);
+                hf_station_dict.TryGetValue(id_num, out var idName);
+                syName ??= "[未定義]";
+                ikName ??= "[未定義]";
+                idName ??= "[未定義]";
+                var line = $"{dt},{key},{id},{bs},{sy},{ik},{dl},{hk}\n";
+                Console.WriteLine($"  [{key}] {no} id={id}/{(!isE ? "前駅～" : "")}{idName}{(isU ? "(上り)" : isD ? "(下り)" : "")} bs={bs} sy={sy}/{syName} ik={ik}/{ikName} dl={dl} hk={hk}");
+                AddCsv(no, line);
+            }
         }
-    }
+    Console.WriteLine();
+
 }
+
 
 void AddCsv(string no, string line)
 {
-    var path = "log\\hapi\\" + no + ".csv";
-    Directory.CreateDirectory("log\\hapi");
+    var eDt = DateTime.Now - TimeSpan.FromHours(3);//25時までカウント
+    var dir = $"log\\hapi\\{eDt:yyyyMM}\\{eDt:dd}";
+    Directory.CreateDirectory(dir);
+    var path = Path.Combine(dir, no + ".csv");
     if (File.Exists(path))
     {
-        var csv = File.ReadAllText(path);
-        File.WriteAllText(path, csv + line);
-
+        File.AppendAllText(path, line);
     }
     else
     {
-        File.WriteAllText(path, "dt,TS/EK,id,no,bs,sy,ik,dl,hk");
+        File.WriteAllText(path, "dt,TS/EK,id,no,bs,sy,ik,dl,hk\n" + line);
     }
     return;
 }
